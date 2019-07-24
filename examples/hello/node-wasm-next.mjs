d7dc0f68fe095bfc28c6121d7f2df85b9bbc8f3e
César Carruitero <ccarruitero@protonmail.com>
2019-07-24 16:48:21 -0500

    fix node experimental-wasm-module example

--- examples/hello/node-wasm-next.mjs
+++ examples/hello/node-wasm-next.mjs
@@ -1,6 +1,3 @@
 import * as wasmModule from './lib.wasm';
 
-(async () => {
-  // const wasmModule = await import('./lib.wasm');
-  console.log(wasmModule.add_one(10));
-});
+console.log(wasmModule.add_one(10));
